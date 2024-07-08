7b2e05837f6387d449a51cc285ca41ec94659d27
yasmin_perette <yasmin_perette@SN-360374.edu.fiesc.com.br>
2024-07-08 20:13:07 -0300

    1° Atualização

--- GitHub2°/src/GitHub/GitHub2.java
+++ GitHub2°/src/GitHub/GitHub2.java
@@ -4,7 +4,7 @@ public class GitHub2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println ("OII DEUS, SOU EU DE NOVO");
+		System.out.println ("OII DEUS, SOU EU DE NOVO!");
 
 	}
 
